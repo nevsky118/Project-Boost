04f103d3075553e30c74778f4a74f88370e6ce58
kringel118 <25124427+nevsky118@users.noreply.github.com>
2020-11-01 01:15:00 +0300

    01/11/2020

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,38 +2,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] float rotationSpeed = 20f;
-    [SerializeField] float thrusterSpeed = 2f;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float thrusterSpeed = 2.5f;
     Rigidbody rigidbody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Rotate();
+        Thrust();
     }
 
-    private void ProcessInput()
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; // take manual control of rotation
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+        rigidbody.freezeRotation = false; // resume physics control of rotation
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up * thrusterSpeed);
+            if (!audioSource.isPlaying)
+                audioSource.Play();
         }
-        if (Input.GetKey(KeyCode.D))
+        else
         {
-            transform.Rotate(-Vector3.forward * Time.deltaTime * rotationSpeed);
+            audioSource.Stop();
         }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
-        }
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("ok");
+                break;
+            case "Finish":
+                print("finish");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                break;
+            case "Enemy":
+                print("Dead");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+               // Destroy(gameObject);
+                break;
+            default:
+                break;
+        }
     }
 }
